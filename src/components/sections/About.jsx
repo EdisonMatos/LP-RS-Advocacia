c6d0724fe7bdb9e41e915e6c3a01ee6440292544
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-18 09:38:21 -0300

    fix: Galeria de imagens no About

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -13,7 +13,6 @@ import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import img1 from "../../assets/imgs/about/image1.png";
 import img2 from "../../assets/imgs/about/image2.png";
-import img3 from "../../assets/imgs/about/image3.png";
 
 export default function About({
   modal = true,
@@ -44,10 +43,6 @@ export default function About({
       original: img2,
       thumbnail: img2,
     },
-    {
-      original: img3,
-      thumbnail: img3,
-    },
   ];
 
   return (
@@ -56,15 +51,15 @@ export default function About({
       className={`${bgClass} transition-colors duration-1000`}
     >
       <SectionWrapper className="flex flex-col desktop1:flex-row-reverse gap-[40px] desktop1:gap-x-[40px] desktop2:gap-0 desktop1:justify-between">
-        <MotionDivDownToUp className="relative w-[90%] desktop1:w-[415px] desktop2:w-[450px] flex justify-center rounded-xl">
+        {/* <MotionDivDownToUp className="relative w-[90%] desktop1:w-[415px] desktop2:w-[450px] flex justify-center rounded-xl">
           <img
             src={content.texts.about.imagem.img}
             alt={content.texts.about.imagem.alt}
             className="w-[100%] desktop1:w-[415px] desktop2:w-[485px] rounded-xl shadow-custom-opacity shadow-primary/25 "
           />
-        </MotionDivDownToUp>
-        {/* <MotionDivDownToUp className=" w-[100%] desktop1:w-[415px] desktop2:w-[485px] flex justify-center">
-          <div className="shadow-custom-opacity shadow-secondary/25 relative bg-center bg-no-repeat bg-cover h-[350px] w-[90%] tablet1:w-full tablet1:h-[740px] desktop1:h-[467px] rounded-xl">
+        </MotionDivDownToUp> */}
+        <MotionDivDownToUp className=" w-[100%] desktop1:w-[415px] desktop2:w-[485px] flex justify-center">
+          <div className="shadow-custom-opacity shadow-secondary/25 relative bg-center bg-no-repeat bg-cover h-auto w-full tablet1:w-full rounded-xl">
             <ImageGallery
               items={images}
               showNav={false} // Ativando a navegação
@@ -77,35 +72,35 @@ export default function About({
             />
             <style>
               {`
-                    .custom-gallery .image-gallery-slide img {
-                      height: 350px; 
-                      width: 100%;
-                      object-fit: cover;
-                      border-radius: 10px;
-                    }
+                .custom-gallery .image-gallery-slide img {
+                  min-height: 450px !important;
+                  width: 100% !important;
+                  object-fit: cover !important;
+                  border-radius: 10px !important;
+                  margin: 0 auto;
+                }
 
-                     @media (min-width: 640px) and (max-width: 1023px) {
-                        .custom-gallery .image-gallery-slide img {
-                          height: 800px;
-                        }
-                      }
+                @media (min-width: 640px) and (max-width: 1023px) {
+                  .custom-gallery .image-gallery-slide img {
+                    min-height: 840px !important;
+                  }
+                }
 
-                      @media (min-width: 1024px) {
-                        .custom-gallery .image-gallery-slide img {
-                          height: 470px;
-                        }
-                      }
+                @media (min-width: 1024px) {
+                  .custom-gallery .image-gallery-slide img {
+                    height: 600px !important;
+                  }
+                }
 
-
-                    .custom-gallery .image-gallery-thumbnails img {
-                      height: 60px;  
-                      width: 100px;  
-                      object-fit: cover; 
-                    }
-                  `}
+                .custom-gallery .image-gallery-thumbnails img {
+                  height: 60px !important;
+                  width: 100px !important;
+                  object-fit: cover !important;
+                }
+              `}
             </style>
           </div>
-        </MotionDivDownToUp> */}
+        </MotionDivDownToUp>
         <div className="desktop1:w-[550px] desktop2:w-[570px]">
           <SectionHeader
             className="text-center"
